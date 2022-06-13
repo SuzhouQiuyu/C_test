15e580d53fe48708ed4e8a50cb5d72641ceb90f1
SuzhouQiuyu <zhongtiaozhilian@163.com>
2022-06-13 23:22:54 +0800

    test4

---
+++ test4_1.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int binary_search(int m, int n, arr1[])
+{
+
+}
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9 };
+	int a = 7;
+	int len = sizeof(arr) / sizeof(arr[0]);
+	int b = binary_search(a, len, arr);
+	if (b = 0)
+	{
+		printf("ц╩спур╣╫");
+	}
+	else
+	{
+		printf("%d", b);
+	}
+}
