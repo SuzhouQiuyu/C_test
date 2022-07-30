944afac259d3be2e8f92457d44583be309916421
SuzhouQiuyu <zhongtiaozhilian@163.com>
2022-07-30 18:18:36 +0800

    test26

---
+++ test26.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int main()
+//{
+//	int a = 1;   //0x00000001
+//	char* p = (char*)&a;  //强制转换为char类型。指针*p存储地址，值不变，类型改变
+//	if (*p == 1)
+//	{
+//		printf("小端");
+//	}
+//	else 
+//	{
+//		printf("大端");
+//	}
+//	return 0;
+//}
+//int check_sys(int a)
+//{
+//	char* p = &a;
+//	if (*p == 1)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//}
+//int main()
+//{
+//	int a = 1;
+//	int ret = check_sys(a);
+//	if (ret == 1)
+//	{
+//		printf("小端");
+//	}
+//	else if(ret == 0)
+//	{
+//		printf("大端");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	char a = -1;
+//	signed char b = -1;
+//	unsigned char c = -1;
+//	printf("a = %d,b = %d,c = %d",a,b,c);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char a = 128;
+//	printf("a = %u", a);
+//	return 0;
+//}
+
