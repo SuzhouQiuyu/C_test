6df29ad0813344d013c0639a8857c6c96ecec9bf
SuzhouQiuyu <zhongtiaozhilian@163.com>
2022-08-08 23:09:51 +0800

    test30

---
+++ test30.c
@@ -0,0 +1,105 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;
+//	char b = '1';
+//	void* c = &pa;
+//	void* d = &b;
+//	return 0;
+//}	
+
+
+#include <stdlib.h>
+#include <string.h>
+
+void bubble_sort(int arr[], int sz)
+{
+	int i = 0;  //趟数
+	for (i = 0; i < sz - 1; i++)
+	{
+		//一趟冒泡排序
+		int j = 0;
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+
+			}
+		}
+	}
+}
+
+struct Stu
+{
+	char name[20];
+	int age;
+};
+
+int cmp_int(const void* e1, const void* e2)
+{
+	return *(int*)e1 - *(int*)e2;  
+	//比较两个整型值时需要强制类型转换，void* 不可以解引用
+}
+
+void test1()
+{
+	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	qsort(arr, sz, sizeof(arr[0]), cmp_int);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+
+int cmp_float(const void* e1, const void* e2)
+{
+	return ((int)(*(float*)e1 - *(float*)e2));
+	//(*(float*)e1 - *(float*)e2)相减结果再强制类型转换为int
+}
+
+void test2()
+{
+	float f[] = { 9.0,8.0,7.0,6.0,5.0,4.0,3.0,2.0,1.0 };
+	int sz = sizeof(f) / sizeof(f[0]);
+	qsort(f, sz, sizeof(f[0]), cmp_float);
+	int j = 0;
+	for (j = 0; j < sz; j++)
+	{
+		printf("%f ", f[j]);
+	}
+}
+
+int cmp_stu_by_age(const void* e1, const void* e2)
+{
+	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+}
+
+int cmp_stu_by_name(const void* e1, const void* e2)
+{
+	return strcmp(((struct Stu*)e1)->name, ((struct Stu*)e2)->name);
+}
+
+void test3()
+{
+	struct Stu s[3] = { {"zhangsan",20},{"lisi",22},{"wangwu",19} };
+	int sz = sizeof(s) / sizeof(s[0]);
+	qsort(s, sz, sizeof(s[0]), cmp_stu_by_age);
+	qsort(s, sz, sizeof(s[0]), cmp_stu_by_name);
+}
+
+int main()
+{
+	test1();
+	printf("\n");
+	test2();
+	test3();  
+	return 0;
+}
